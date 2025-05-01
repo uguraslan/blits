0053a10f55ec074a37e14060fa467f7f8743cd41
Ugur Aslan <ugur@uguraslan.net>
2025-05-01 14:45:49 +0200

    fixed linting issue

--- src/launch.js
+++ src/launch.js
@@ -16,20 +16,11 @@
  */
 
 import Settings from './settings.js'
-import { initLog, 
-
-	Log } from './lib/log.js'
+import { initLog, Log } from './lib/log.js'
 import engine from './engine.js'
 import blitsPackageInfo from '../package.json' assert { type: 'json' }
 
-export let renderer = {
-
-
-
-
-
-
-}
+export let renderer = {}
 export const stage = {}
 
 async function rendererVersion() {
